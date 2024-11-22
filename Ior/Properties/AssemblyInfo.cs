c59263b1b97521efbf21ee8fb1e07366ed17edc8
Eduard Islamov <eaisla10@mts.ru>
2024-11-22 16:06:31 +0300

    Version info update

--- Ior/Properties/AssemblyInfo.cs
+++ Ior/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("I'm Only Resting")]
-[assembly: AssemblyDescription("http://www.swensensoftware.com/im-only-resting")]
+[assembly: AssemblyDescription("https://github.com/alviy/im-only-resting")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("I'm Only Resting")]
-[assembly: AssemblyCopyright("Copyright © Swensen Software 2012 - 2016")]
+[assembly: AssemblyCopyright("©Swensen 2012-2016, Github Community 2026-2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -22,4 +22,4 @@ using System.Runtime.InteropServices;
 [assembly: Guid("42d77ebc-de54-4916-b931-de852d2bacff")]
 
 //use odd revision number (fourth position) to indicate pre-release
-[assembly: AssemblyVersion("2.0.0.3")]
+[assembly: AssemblyVersion("2.0.1.0")]
